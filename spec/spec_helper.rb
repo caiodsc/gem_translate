d328ab35718fc77ef081438b8caa260954a52cee
caiodsc <caio.dscamara@gmail.com>
2018-07-20 09:27:44 -0400

    Tradutor + testes com rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require "bundler/setup"
 require "translate"
 
 RSpec.configure do |config|
+  TEST_API_KEY = "trnsl.1.1.20180720T131759Z.b064fbea7067b3ca.10f0f2ed81f97017be118fbaf41d2780a92b5d3d"
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
@@ -12,3 +13,4 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 end
+
--- spec/translate_spec.rb
+++ spec/translate_spec.rb
@@ -1,9 +1,46 @@
 RSpec.describe Translate do
-  it "has a version number" do
-    expect(Translate::VERSION).not_to be nil
+  describe "GET api/v1.5/tr.json/translate (Translate)" do
+    # Teste 1 (Translate)
+    it "Api response == 200" do
+      textPt = "Nós estamos testando o tradutor"
+      yandexApi = Translate::Yandex.new TEST_API_KEY
+      response = yandexApi.translate textPt, "en"
+      expect(response["code"]).to eq(200)
+    end
+
+    it "translated text match" do
+      textPt = "Nós estamos testando o tradutor"
+      textEn = "We are testing the translator"
+      yandexApi = Translate::Yandex.new TEST_API_KEY
+      response = yandexApi.translate textPt, "en"
+
+      expect(response["text"].first).to eq(textEn)
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+# Teste 2 (supported list of languages)
+  describe "GET api/v1.5/tr.json/getLangs (Supported List)" do
+    it "Api response not equal nil or empty '' " do
+      yandexApi = Translate::Yandex.new TEST_API_KEY
+      response = yandexApi.list
+      expect(response).not_to eq(nil)
+      expect(response).not_to eq("")
+    end
+  end
+
+  describe "GET api/v1.5/tr.json/detect (Detect)" do
+    it "Api response == 200" do
+      textPt = "Nós estamos testando o tradutor"
+      yandexApi = Translate::Yandex.new TEST_API_KEY
+      response = yandexApi.detect textPt
+      expect(response["code"]).to eq(200)
+    end
+
+    it "detect language match with right language" do
+      textPt = "Nós estamos testando o tradutor"
+      yandexApi = Translate::Yandex.new TEST_API_KEY
+      response = yandexApi.detect textPt
+      expect(response["lang"]).to eq("pt")
+    end
   end
 end
